8670ad0968feba031b13a06d9be9ded96130c367
Misael Alberto <misael.alberto@outlook.com>
2023-08-02 09:03:28 -0300

    inserindo frontend com js vanilla

--- src/main/java/com/br/estudos/Api/controllers/ProfissionaisController.java
+++ src/main/java/com/br/estudos/Api/controllers/ProfissionaisController.java
@@ -4,6 +4,7 @@ import java.lang.reflect.InvocationTargetException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -22,6 +23,7 @@ import com.br.estudos.Api.models.Profissional;
 import com.br.estudos.Api.servicos.BuilderDTOModel;
 import com.br.estudos.Api.servicos.GenericBuilderDTOModel;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/profissionais")
 public class ProfissionaisController {
